e3baca05b3052b388ba1156598acaea760fbad5d
A11 <A11>
2018-06-10 16:14:26 +0700

    first commit

---
+++ app/src/main/java/com/example/pc/firstkotlin/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.pc.firstkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.gson.Gson
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+
+}
